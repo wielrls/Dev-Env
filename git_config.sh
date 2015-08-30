f765350843b7edffe97b38f5d01e3699beb069e3
Neville Bowers <neville.bowers@gmail.com>
2015-08-29 17:48:18 -0700

    Quick Update

--- git_config.sh
+++ git_config.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+#
+# Setup:
+# Run this file once
+#
+
 # Configure common aliases
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+
+# Configure editor
+git config --global core.editor vim
+
+# Configure commit template
+# TBD
