25c02d4f0e0163f5c92baa473ba41670b03c8a06
Neville Bowers <neville.bowers@gmail.com>
2016-09-20 21:54:37 -0700

    Update bash_config.sh

--- bash_config.sh
+++ bash_config.sh
@@ -21,6 +21,7 @@ export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
 #
 alias g='git'
 alias gg='git grep -n'
+alias nr='npm run'
 alias v='vim'
 
 if [[ `uname` == 'Darwin' ]]; then
