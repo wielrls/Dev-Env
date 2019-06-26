d19a77e990905794d250e2fe392fd4d826cc7395
Neville Bowers <nsb@rimeto.io>
2019-06-26 11:34:16 -0700

    fix timestamp

--- bash_config.sh
+++ bash_config.sh
@@ -14,7 +14,7 @@
 parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-export PS1="\u@\h \e[0;34m\t \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+export PS1="[\t] \u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
 
 #
 # Useful aliases
