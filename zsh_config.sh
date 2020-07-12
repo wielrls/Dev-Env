d26924295bfeb549ea505ef624ecc10cbbc373b2
Neville Bowers <nbowers@slack-corp.com>
2020-07-11 18:03:18 -0700

    Add zsh_config.sh

---
+++ zsh_config.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#
+# Setup:
+# Add the following line to ~/.zshrc
+# source "$PATH_TO_HERE/zsh_config.sh"
+#
+
+#
+# Configuration
+#
+setopt AUTO_CD
+HISTFILE=${ZDOTDIR:-$HOME}/.zsh_history
+setopt EXTENDED_HISTORY
+setopt SHARE_HISTORY
+setopt APPEND_HISTORY
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_FIND_NO_DUPS
+setopt HIST_REDUCE_BLANKS
+setopt CORRECT
+setopt CORRECT_ALL
+
+#
+# Useful aliases
+#
+alias less='less -R'
+alias gg='git grep -n'
+alias v='vim'
+alias y='yarn'
+
+if [[ `uname` == 'Darwin' ]]; then
+  alias ls='ls -G'
+else
+  alias ls='ls --color=auto'
+fi
+
